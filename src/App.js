ddf68e82add3dac7d7fa8c5e27e06f4e95051ec2
Nicole Olliver <noliver.ykc@gmail.com>
2022-09-16 01:21:56 +0900

    added use reducer (instead of use state) to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useReducer } from 'react';
 import './styles.css';
 
+function reducer(state, action) {
+
+}
+
 function App() {
+  const[{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123dddddd5 *</div>
-        <div className="current-operand">35</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two operator">AC</button>
       <button className='operator'>DEL</button>
